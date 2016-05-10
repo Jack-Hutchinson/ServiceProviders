6e5dfe7db2dfd5c9f04f819495d7fc9e9f0d49e9
Jack-Hutchinson <jack_hutchinson@hotmail.com>
2016-05-10 10:26:51 -0700

    Initial Commit

---
+++ AngiesListCodingChallenge/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AngiesListCodingChallenge
+//
+//  Created by Jack Hutchinson on 5/10/16.
+//  Copyright © 2016 Helium Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AngiesListCodingChallenge/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AngiesListCodingChallenge
+//
+//  Created by Jack Hutchinson on 5/10/16.
+//  Copyright © 2016 Helium Apps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
